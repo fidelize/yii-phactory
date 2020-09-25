6bbf6b49e28edd231e6e08deeec8449e2a841ba6
Alessandra <aleterre10@gmail.com>
2020-09-25 17:59:55 -0300

    Added private and protected manipulation
    Added executePrivateMethod and getPrivatePropertyValue methods in FPhactoryTestCase.

--- src/FPhactoryTestCase.php
+++ src/FPhactoryTestCase.php
@@ -2,6 +2,7 @@
 namespace fidelize\YiiPhactory;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 
 abstract class FPhactoryTestCase extends TestCase
 {
@@ -33,5 +34,32 @@ abstract class FPhactoryTestCase extends TestCase
     {
         parent::tearDown();
         $this->_transaction->rollBack();
+    }
+    
+    /**
+     * @param object $classObject
+     * @param string $property
+     * This method is used to get private AND protected class property values
+     */
+    protected function getPrivatePropertyValue($classObject, $property)
+    {
+        $reflection = new ReflectionClass($classObject);
+        $reflection_property = $reflection->getProperty($property);
+        $reflection_property->setAccessible(true);
+        return $reflection_property->getValue($classObject);
+    }
+    
+    /**
+     * @param object $classObject
+     * @param string $methodName
+     * @param array $args
+     * This method is used to execute private AND protected methods
+     */
+    protected function executePrivateMethod($classObject, $methodName, $args)
+    {
+        $reflection = new ReflectionClass($classObject);
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($classObject, $args);
     }
 }
